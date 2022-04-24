4ef242eb30b44ca10425444dbbf1f30cc7d170e6
shreyas97049 <59139818+shreyas97049@users.noreply.github.com>
2022-04-24 18:21:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import './App.css';
+import Form from "./Form"
+ 
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+     
+  );
+}
+ 
+export default App;
